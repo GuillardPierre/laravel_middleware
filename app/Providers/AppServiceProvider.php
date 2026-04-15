7097edbdee4025cde34d8388ff844bacc3417b23
PierreGllrd <pguillard95@gmail.com>
2026-04-15 10:23:26 +0200

    Implement gates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
+
+        Gate::define('view-product', function (User $user, Product $product): bool {
+            return $user->id === $product->user_id || $product->is_public;
+        });
+
+        Gate::define('manage-product', function (User $user, Product $product): bool {
+            return $user->id === $product->user_id;
+        });
     }
 }
